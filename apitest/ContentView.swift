99cfe2b4f4924aad6713f820ca8afa0cc52accdb
alberto Juarbe <albertojuarbe2@gmail.com>
2021-12-14 22:20:03 -0500

    Initial Commit

---
+++ apitest/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  apitest
+//
+//  Created by Alberto Juarbe on 12/14/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ apitest/apitestApp.swift
@@ -0,0 +1,17 @@
+//
+//  apitestApp.swift
+//  apitest
+//
+//  Created by Alberto Juarbe on 12/14/21.
+//
+
+import SwiftUI
+
+@main
+struct apitestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
